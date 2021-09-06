332ee1db842674fa06fb61b163610f60533f6059
EVOAUCTION <89047635+EVOAUCTION@users.noreply.github.com>
2021-09-06 14:08:51 -0400

    Update next.config.js

--- js/packages/web/next.config.js
+++ js/packages/web/next.config.js
@@ -10,7 +10,7 @@ const plugins = [
       lessLoaderOptions: {
         lessOptions: {
           modifyVars: {
-            '@primary-color': '#768BF9',
+            '@primary-color': '#00FFA3',
             '@text-color': 'rgba(255, 255, 255)',
             '@assetPrefix': assetPrefix || "''",
           },
